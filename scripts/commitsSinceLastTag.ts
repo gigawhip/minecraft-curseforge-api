be60bf35c3c3fdc9e904cb710fbe7ecf5b578c45
gigawhip <gigawhip@gmail.com>
2023-03-26 11:57:01 -0700

    chore: extract cmd util for repo scripts

--- scripts/commitsSinceLastTag.ts
+++ scripts/commitsSinceLastTag.ts
@@ -1,16 +1,10 @@
 import clipboard from "https://deno.land/x/clipboard@v0.0.2/mod.ts";
 
-const tag = await Deno.run({
-  cmd: ["git", "describe", "--tags", "--abbrev=0"],
-  stdout: "piped",
-}).output()
-  .then((output) => new TextDecoder().decode(output).trim());
+import { cmd } from "./utils/cmd.ts";
 
-await Deno.run({
-  cmd: ["git", "log", `${tag}..HEAD`, "--oneline"],
-  stdout: "piped",
-}).output()
-  .then((output) => new TextDecoder().decode(output))
+const tag = await cmd("git describe --tags --abbrev=0");
+
+await cmd(`git log ${tag}..HEAD --oneline`)
   .then((output) => clipboard.writeText(output));
 
 console.log(`Commits since ${tag} copied to clipboard`);
---
+++ scripts/utils/cmd.ts
@@ -0,0 +1,6 @@
+/** Run a terminal command then return stdout, trimmed, as a string. */
+export async function cmd(command: string): Promise<string> {
+  return Deno.run({ cmd: command.split(" "), stdout: "piped" })
+    .output()
+    .then((output) => new TextDecoder().decode(output).trim());
+}
